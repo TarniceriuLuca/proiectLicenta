255abcdeb548a1a025e4cc7b9756583171b16f7f
Luca Tărniceriu <165776962+TarniceriuLuca@users.noreply.github.com>
2026-03-16 18:28:33 +0200

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -6,23 +6,16 @@ total, used, free = shutil.disk_usage("/")
 
 # print("Disk Used: %d GiB" % (used // (2**30)))
 
-
-def systemInfo():
-    # values represent in order: platform, cpuBrand, memCap, diskSize
-    return (
-            '[' +
-            str(platform.system()) + ',' +
-            str(cpuinfo.get_cpu_info()['brand_raw']) + ',' +
-            str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + ',' +
-            str(total // (2 ** 30)) +
-            ']'
-            )
 def updateInfo():
-    # values reprezent in order: memory%, cpu%, cpu frequency, last reboot time
+    # values reprezent in order: memory%, cpu%, cpu frequency, last reboot time, platform, cpuBrand, memCap, diskSize
     return (
             '[' +
             str(psutil.virtual_memory().percent) + ',' +
             str(psutil.cpu_percent()) + ',"' +
-            str(datetime.datetime.fromtimestamp(psutil.boot_time())) +
+            str(datetime.datetime.fromtimestamp(psutil.boot_time())) + ',' +
+            str(platform.system()) + ',' +
+            str(cpuinfo.get_cpu_info()['brand_raw']) + ',' +
+            str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + ',' +
+            str(total // (2 ** 30)) +
             '"]'
             )
